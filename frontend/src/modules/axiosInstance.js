b2b534b976b2e8fd31903477ecb46507fdbbefb1
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 19:31:07 -0400

    dasdas

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'https://gullible-rifle.railway.internal'
+    baseURL: 'https://gullible-rifle'
 })
 
 export default instance
