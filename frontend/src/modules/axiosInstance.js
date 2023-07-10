a9509ae24e661df752caf2a8b58849362039a89c
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 17:29:48 -0400

    fix typo

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://gullible-rifle-production.up.railway.app'
+    baseURL: 'https://gullible-rifle-production.up.railway.app'
 })
 
 export default instance
