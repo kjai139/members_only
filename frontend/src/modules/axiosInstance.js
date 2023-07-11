203c8492f077bfd86dfdb31b83a7f5cf00e8677c
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 22:56:11 -0400

    dsadas

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: `https://youthful-reward`
+    baseURL: `https://youthful-reward-production.up.railway.app`
 })
 
 export default instance
