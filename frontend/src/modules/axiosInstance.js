16bed5ab4c252ac82a8843ef1cfca25504bf46ad
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 20:15:44 -0400

    dasds

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'https://prized-mint.railway.internal'
+    baseURL: 'https://prized-mint.railway.internal:4000'
 })
 
 export default instance
