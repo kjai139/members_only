f64c91675324558d0ab186327dd9107066b394ba
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 22:40:10 -0400

    sdad

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Layout from './components/layout';
 import SignInForm from './components/sign_in_form';
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 
 function App() {
