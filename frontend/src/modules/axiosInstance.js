5238e8089cf6a3a7c22e0e49d6fcaee23b28e5dc
simon y <71997308+kjai139@users.noreply.github.com>
2023-07-10 21:34:41 -0400

    sdasds

--- frontend/src/modules/axiosInstance.js
+++ frontend/src/modules/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'https://prized-mint.railway.internal'
+    baseURL: `https://prized-mint.railway.internal:${process.env.PORT}`
 })
 
 export default instance
